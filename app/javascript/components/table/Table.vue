90a1de3da15f724680f71e11139fdb56f0fa2c40
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-11-16 16:53:25 +0000

    Setup javascript workflow

---
+++ app/javascript/components/table/Table.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>Table</p>
+</template>
+
+<script>
+  export default {
+    name: 'table'
+  }
+</script>
---
+++ app/javascript/store/store.js
@@ -0,0 +1,14 @@
+import { polyfill } from 'es6-promise'
+polyfill()
+
+import Vue from 'vue/dist/vue.esm'
+import Vuex from 'vuex/dist/vuex.esm'
+
+Vue.use(Vuex)
+
+// create store
+export default new Vuex.Store({
+  state: {},
+
+  mutations: {}
+})
