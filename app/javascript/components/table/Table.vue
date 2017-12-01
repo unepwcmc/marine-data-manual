609d9144b83c8894b63141dc39560773e5f48828
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-12-01 14:15:13 +0000

    Add config that compiles css within vue single components

--- app/javascript/components/table/Table.vue
+++ app/javascript/components/table/Table.vue
@@ -1,9 +1,28 @@
 <template>
-  <p>Table</p>
-</template>
+  <div>
+    table
+    <table class="table table--head">
+      <!-- <table-head :filters="filters"></table-head> -->
+    </table>
 
+    <table class="table table--body">
+      <tbody>
+        <!-- <row v-for="item, key in items"
+          :key="key"
+          :item="item">
+         --></row>
+      </tbody>
+    </table>
+  </div>
+</template>
 <script>
   export default {
     name: 'v-table'
   }
 </script>
+
+<style>
+  body {
+    color: red;
+  }
+</style>
