bea15747d779d19bc0f0f37d83218373734495c4
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-12-06 16:15:19 +0000

    Fix variable names for the themes

--- app/javascript/components/table/Row.vue
+++ app/javascript/components/table/Row.vue
@@ -4,16 +4,16 @@
       <td>{{ item.category }}</td>
       <td>{{ trim(item.resource) }}</td>
       <td>{{ item.version }}</td>
-      <td>{{ item.contactOrganisation }}</td>
+      <td>{{ item.contact_organisation }}</td>
       <td>{{ item.id }}</td>
       <td><a v-if="hasMetadata" :href="item.metadata">Link</a></td>
       <td><a v-if="hasFactsheet" :href="item.factsheet">Link</a></td>
       <td>
-        <span v-if="item.marineSpatialPlanning" class="icon--round icon--theme icon--theme-1">MSP</span>
+        <span v-if="item.marine_spatial_planning" class="icon--round icon--theme icon--theme-1">MSP</span>
         <span v-if="item.education" class="icon--round icon--theme icon--theme-2">E</span>
-        <span v-if="item.ecosystemAssessment" class="icon--round icon--theme icon--theme-3">EA</span>
-        <span v-if="item.environmentalImpactAssessment" class="icon--round icon--theme icon--theme-4">EIM</span>
-        <span v-if="item.ecosystemServices" class="icon--round icon--theme icon--theme-5">ES</span>
+        <span v-if="item.ecosystem_assessment" class="icon--round icon--theme icon--theme-3">EA</span>
+        <span v-if="item.environmental_impact_assessment" class="icon--round icon--theme icon--theme-4">EIM</span>
+        <span v-if="item.ecosystem_services" class="icon--round icon--theme icon--theme-5">ES</span>
       </td>
     </tr>
     <tr class="table__row--expandable flex" :class="{ 'table__row--open' : isOpen }">
@@ -26,27 +26,27 @@
         <h3>Themes</h3>
         <div class="flex table__themes">
           <ul class="ul-unstyled flex-2-fiths">
-            <li v-if="item.marineSpatialPlanning" class="table__theme">
+            <li v-if="item.marine_spatial_planning" class="table__theme">
               <span class="icon--round icon--theme icon--theme-1">MSP</span>
-              <span class="table__theme-title">{{ themeName('marineSpatialPlanning') }}</span>
+              <span class="table__theme-title">{{ themeName('marine_spatial_planning') }}</span>
             </li>
             <li v-if="item.education" class="table__theme">
               <span class="icon--round icon--theme icon--theme-2">E</span>
               <span class="table__theme-title">{{ themeName('education') }}</span>
             </li>
-            <li v-if="item.ecosystemAssessment" class="table__theme">
+            <li v-if="item.ecosystem_assessment" class="table__theme">
               <span class="icon--round icon--theme icon--theme-3">EA</span>
-              <span class="table__theme-title">{{ themeName('ecosystemAssessment') }}</span>
+              <span class="table__theme-title">{{ themeName('ecosystem_assessment') }}</span>
             </li>
           </ul>
           <ul class="ul-unstyled flex-3-fiths">
-            <li v-if="item.environmentalImpactAssessment" class="table__theme">
+            <li v-if="item.environmental_impact_assessment" class="table__theme">
               <span class="icon--round icon--theme icon--theme-4">EIM</span>
-              <span class="table__theme-title">{{ themeName('environmentalImpactAssessment') }}</span>
+              <span class="table__theme-title">{{ themeName('environmental_impact_assessment') }}</span>
             </li>
-            <li v-if="item.ecosystemServices" class="table__theme">
+            <li v-if="item.ecosystem_services" class="table__theme">
               <span class="icon--round icon--theme icon--theme-5">ES</span> 
-              <span class="table__theme-title">{{ themeName('ecosystemServices') }}</span>
+              <span class="table__theme-title">{{ themeName('ecosystem_services') }}</span>
             </li>
           </ul>
         </div>
@@ -102,7 +102,7 @@
       },
 
       themeName (name) {
-        return name.replace(/([A-Z])/g, ' $1')
+        return name.replace(/_/g, ' ')
       },
 
       toggleRow () {
