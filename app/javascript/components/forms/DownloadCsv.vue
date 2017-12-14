883eb7f7b5100d5c7509207a71a0baedf32cd90c
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-12-14 14:56:36 +0000

    Add and style download csv button

---
+++ app/javascript/components/forms/DownloadCsv.vue
@@ -0,0 +1,29 @@
+<template>
+  <form action="/download" method="post" class="inline-block">
+    <input name="ids" type="hidden" v-bind:value="postIds"></input>
+    <input 
+      type="submit" 
+      value="CSV" 
+      title="Download CSV file of filtered Coral projects" 
+      class="button button--download button--green filter__download"
+      :class="{ 'button--disabled' : !hasResults }"
+      v-bind="{ 'disabled' : !hasResults }">
+    </input>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'download-csv',
+
+    computed: {
+      postIds () {
+        return this.$store.state.activeItems
+      },
+      
+      hasResults () {
+        return this.$store.state.activeItems.length > 0
+      }
+    }
+  }
+</script>
