f48bf10415584f2481b715685e452994dd60f132
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-12-19 15:06:00 +0000

    Add social icons, style, add urls, sticky component to the side of the page

--- app/controllers/ecosystem_assessment_controller.rb
+++ app/controllers/ecosystem_assessment_controller.rb
@@ -2,7 +2,7 @@ class EcosystemAssessmentController < ThemesController
 
   def index
     @title = helpers.ecosystem_assessment_title
-    @headerClass = helpers.createClass(@title)
+    @headerClass = helpers.create_class(@title)
     @intro = helpers.ecosystem_assessment_intro
     @case_study_intro = helpers.ecosystem_assessment_case_study_intro
     @case_study_bullets = helpers.ecosystem_assessment_case_study_bullets
