7bd00d091568edcf662d1717eae5e689d7c1ac0d
stacytalbot <stacymtalbot@gmail.com>
2019-02-19 17:41:39 +0000

    Post params to /download

--- app/javascript/components/forms/DownloadCsv.vue
+++ app/javascript/components/forms/DownloadCsv.vue
@@ -1,6 +1,6 @@
 <template>
   <a 
-    href="metadata/download" 
+    @click="download()"
     title="Download CSV file" 
     class="inline-block button button--download button--green filter__download">
     CSV
@@ -8,7 +8,32 @@
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
-    name: 'download-csv'
+    name: 'download-csv',
+
+    methods: {
+      download () {
+        let data = {
+          params: {
+            filters: this.$store.state.selectedFilterOptions,
+            items_per_page: this.itemsPerPage,
+            requested_page: this.$store.state.requestedPage,
+            sortDirection: this.$store.state.sortDirection,
+            sortField: this.$store.state.sortField
+          }
+        }
+
+        const csrf = document.querySelectorAll('meta[name="csrf-token"]')[0].getAttribute('content')
+        axios.defaults.headers.common['X-CSRF-Token'] = csrf
+        axios.defaults.headers.common['Accept'] = 'application/json'
+
+        axios.post('/download', data)
+          .catch(function (error) {
+            console.log(error)
+          })
+      }
+    }
   }
 </script>
