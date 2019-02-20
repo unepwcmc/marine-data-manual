656a3e1f3ead6761cadd935dbe63a15c509177ac
lucacug <luca.cugliari@unep-wcmc.org>
2019-02-20 12:19:52 +0000

    add FE bits to fix csv download issue

--- app/javascript/components/forms/DownloadCsv.vue
+++ app/javascript/components/forms/DownloadCsv.vue
@@ -15,7 +15,8 @@
 
     methods: {
       download () {
-        let data = {
+        const csrf = document.querySelectorAll('meta[name="csrf-token"]')[0].getAttribute('content'),
+        data = {
           params: {
             filters: this.$store.state.selectedFilterOptions,
             items_per_page: this.itemsPerPage,
@@ -23,19 +24,65 @@
             sortDirection: this.$store.state.sortDirection,
             sortField: this.$store.state.sortField
           }
+        },
+        config = {
+          headers: {
+            'X-CSRF-Token': csrf,
+            'Accept': 'text/csv',
+            'responseType': 'blob'
+          }
         }
 
-        const csrf = document.querySelectorAll('meta[name="csrf-token"]')[0].getAttribute('content')
-        axios.defaults.headers.common['X-CSRF-Token'] = csrf
-        axios.defaults.headers.common['Accept'] = 'application/json'
+        axios.post('/download', data, config)
+          .then((response) => {
+            const date = new Date().toJSON().slice(0,10),
+              filename = `marine-data-manual-${date}.csv`
 
-        axios.post('/download', data)
-          .then(function (response) {
-            console.log(response);
+            this.createBlob(filename, response.data)
           })
           .catch(function (error) {
             console.log(error)
           })
+      },
+
+      createBlob (filename, data) {
+        let blob = new Blob([data])
+
+        if (typeof window.navigator.msSaveBlob !== 'undefined') {
+          // IE workaround for "HTML7007: One or more blob URLs were
+          // revoked by closing the blob for which they were created.
+          // These URLs will no longer resolve as the data backing
+          // the URL has been freed."
+          window.navigator.msSaveBlob(blob, filename)
+
+        } else {
+          const blobURL = window.URL.createObjectURL(blob),
+            tempLink = document.createElement('a')
+
+          // Safari thinks _blank anchor are pop ups. We only want to set _blank
+          // target if the browser does not support the HTML5 download attribute.
+          // This allows you to download files in desktop safari if pop up blocking
+          // is enabled.
+          if (typeof tempLink.download === 'undefined') {
+              tempLink.setAttribute('target', '_blank')
+          }
+
+          tempLink.href = blobURL
+          tempLink.setAttribute('download', filename)
+          this.simulateClick(tempLink)
+          window.URL.revokeObjectURL(blobURL)
+        }
+      },
+
+      simulateClick (element) {
+        // created because standard .click() doesn't work in Firefox
+        const event = new MouseEvent('click', {
+          bubbles: true,
+          cancelable: true,
+          view: window
+        })
+        // If cancelled, don't dispatch our event
+        var canceled = !element.dispatchEvent(event)
       }
     }
   }
