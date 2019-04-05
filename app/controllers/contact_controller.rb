f7e5178a70fb5188295848074b932dfb0edf55ff
Will Kocur <will.kocur@unep-wcmc.org>
2019-04-05 11:52:55 +0100

    unstyled form

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,67 @@
 class ContactController < ApplicationController
   def index
+    @contact_form = [
+      {
+        title: 'Resource details',
+        fields: [
+          {
+            id: 'resource_name',
+            label: 'Resource name',
+            type: 'text_field',
+            required: true
+          },
+          {
+            id: 'resource_location',
+            label: 'Resource location',
+            type: 'text_field',
+            required: true,
+            comment: 'Please enter URL link'
+          },
+          {
+            id: 'licence_link',
+            label: 'Link to licence',
+            type: 'text_field',
+            required: true,
+            comment: 'Please enter URL link'
+          },
+          {
+            id: 'contact_organisation',
+            label: 'Contact organisation',
+            type: 'text_field'
+          },
+          {
+            id: 'contact_organisation_email',
+            label: 'Contact organisation email',
+            type: 'email_field',
+            placeholder: 'example@email.com'
+          },
+          {
+            id: 'additional_info',
+            label: 'Additional information',
+            type: 'text_area'
+          },
+        ]
+      },
+      {
+        title: 'Contact details',
+        fields: [
+          {
+            id: 'name',
+            label: 'Name',
+            type: 'text_field',
+          },
+          {
+            id: 'organisation',
+            label: 'Organisation',
+            type: 'text_field',
+          },
+          {
+            id: 'email',
+            label: 'Email address',
+            type: 'email_field',
+          },
+        ]
+      }
+    ]
   end
 end
--- app/helpers/contact_helper.rb
+++ app/helpers/contact_helper.rb
@@ -1,2 +1,9 @@
 module ContactHelper
+  def field_label field
+    label = field[:label]
+    label += '<em class="form__required">*</em>' if field[:required]
+    label += "<em> (#{field[:comment]})</em>" if field[:comment]
+
+    return label
+  end
 end
