5de73a182a6024e4341b39a700b1083a0cd59757
stacytalbot <stacymtalbot@gmail.com>
2019-06-05 13:10:22 +0100

    Update config for webpack 4

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,7 @@
 const { environment } = require('@rails/webpacker')
-const merge = require('webpack-merge')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
-const myCssLoaderOptions = {
-  modules: true,
-  sourceMap: true,
-  localIdentName: '[name]__[local]___[hash:base64:5]'
-}
-
-const CSSLoader = environment.loaders.get('style').use.find(el => el.loader === 'css-loader')
-
-CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions)
-
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
