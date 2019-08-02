a6464609d20a9341962f4a7410a7b913f2dba0a2
stacytalbot <stacymtalbot@gmail.com>
2019-08-02 17:10:59 +0100

    Set up project to use locales folder for content

--- app/controllers/education_controller.rb
+++ app/controllers/education_controller.rb
@@ -1,14 +1,14 @@
 class EducationController < ThemesController
 
   def index
-    @title = helpers.education_title
+    @id = 'education'
     @tagline = helpers.education_tagline
     @headerClass = helpers.education_class
     @intro = helpers.education_intro
     @case_study_intro = helpers.education_case_study_intro
     @case_study_bullets = helpers.education_case_study_bullets
     @resources = helpers.education_resources
-    @theme = helpers.remove_spaces(@title)
+    @theme = 'education'
   end
 
 end
