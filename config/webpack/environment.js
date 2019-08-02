cf7a2fa6f9bf7d57d4dcef7da01447aa4e30e155
stacytalbot <stacymtalbot@gmail.com>
2019-08-02 13:54:35 +0100

    Remove commented out code

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,19 +1,8 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
-// const merge = require('webpack-merge')
-
-// const myCssLoaderOptions = {
-//   modules: true,
-//   sourceMap: true,
-//   localIdentName: '[name]__[local]___[hash:base64:5]'
-// }
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
-
-// const CSSLoader = environment.loaders.get('style').use.find(el => el.loader === 'css-loader')
-
-// CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions)
 
 module.exports = environment
